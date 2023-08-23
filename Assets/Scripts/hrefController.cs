3fcdf1c9919d4f473976f600e434431584071d1a
Vanterric <derrickgallegos@rocketmail.com>
2023-08-23 17:12:44 -0400

    Resized Assets and Added link to insta

---
+++ Assets/Scripts/hrefController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hrefController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void goToInsta()
+    {
+        System.Diagnostics.Process.Start("https://www.instagram.com/deltasigfiu/");
+    }
+
+    public void goToFund()
+    {
+
+    }
+
+    public void goToMainSite()
+    {
+
+    }
+}
