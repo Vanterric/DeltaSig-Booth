47f49b650cf4f44c19c9f7ad0abdee9f11dd2b56
Vanterric <derrickgallegos@rocketmail.com>
2023-08-28 12:10:17 -0400

    Added second panel

--- Assets/Scripts/hrefController.cs
+++ Assets/Scripts/hrefController.cs
@@ -7,16 +7,16 @@ public class hrefController : MonoBehaviour
     // Start is called before the first frame update
     public void goToInsta()
     {
-        System.Diagnostics.Process.Start("https://www.instagram.com/deltasigfiu/");
+        Application.OpenURL("https://www.instagram.com/deltasigfiu/");
     }
 
     public void goToFund()
     {
-
+        Application.OpenURL("https://www.gofundme.com/f/ymr8vf-the-red-cross?utm_source=customer&utm_medium=copy_link&utm_campaign=p_cf+share-flow-1");
     }
 
     public void goToMainSite()
     {
-
+        Application.OpenURL("https://www.deltasigfiu.com/");
     }
 }
